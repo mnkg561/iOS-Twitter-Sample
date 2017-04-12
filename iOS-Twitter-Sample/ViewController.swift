c401061bbc619bb77e96f76bc7be00e80af4c3d7
Naveen Mogulla <nmogulla@mtvl15367e28a.local>
2017-04-11 18:19:40 -0700

    Initial Commit

---
+++ iOS-Twitter-Sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS-Twitter-Sample
+//
+//  Created by Mogulla, Naveen on 4/11/17.
+//  Copyright © 2017 Mogulla, Naveen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
